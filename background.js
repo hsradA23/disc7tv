630cb72a22b00e61b9c0e655eef9bd2bb1eb02c7
Adarsh Singh <hsrada23@gmail.com>
2022-01-02 23:24:06 +0530

    Added emotes

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-let emotes = new Map()
-emotes.set("AlienPls3",  "https://cdn.7tv.app/emote/6186b9574ea2f24e50099c0c/1x")
-emotes.set("ModCheck", "https://cdn.7tv.app/emote/60abf171870d317bef23d399/1x")
-emotes.set("WidelyBased", "https://cdn.7tv.app/emote/60bd587060b022372c085417/1x")
-
-
 let replace = (originalMessage) => {
     let words = originalMessage.split(" ")
     for(let i = 0; i < words.length; i++){
